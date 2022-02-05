a30cfb81752281fd62fcb5d975d00c2bf4a3c6a9
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-05 12:57:42 -0500

    implemented function to recursively sum any level of nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  if (Array.isArray(array[0])){
+    return sumItems(array[0]) + sumItems(array.slice(1));
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  if (array.length === 1) {
+    return array[0];
+  }
+  return array[0] + sumItems(array.slice(1));
 }
 
 module.exports = sumItems;
